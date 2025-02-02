e791f1ffb14c234dc6bb34f2de59266eeb31744b
rofiariansyah <rofiariansyah29@gmail.com>
2025-02-02 23:55:58 +0700

    bismillah

--- flaskdiabet-master/venv/Scripts/activate.bat
+++ flaskdiabet-master/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\MACHINE LEARNING\flaskdiabet-master\venv
+set VIRTUAL_ENV=D:\Prediksi_Diabetes\flaskdiabet-master\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
